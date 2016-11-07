637ba9d6ebb2ef885efc698c27db6de8936dc811
zhoujunzhuo <750907624@qq.com>
2016-11-07 17:34:30 +0800

    LotterySystem

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(396, 82);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(396, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "点击抽奖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(396, 331);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public List<string> CandidateList;
+        public int index;
+        public List<WinningPeople> WinningPeoples = new List<WinningPeople>();
+
+        public Form1()
+        {
+
+            string userStr = ConfigurationSettings.AppSettings["Users"];
+            string WinningPeoplesCount = ConfigurationSettings.AppSettings["LotteryNumber"];
+            if (userStr != null && WinningPeoplesCount != null)
+            {
+                CandidateList = userStr.Split(',').ToList();
+                var lotteryNumber = int.Parse(WinningPeoplesCount) > CandidateList.Count() ? CandidateList.Count() : int.Parse(WinningPeoplesCount);
+                for (int i = 0; i < lotteryNumber / 2; i++)
+                {
+                    WinningPeoples.Add(new WinningPeople());
+                }
+            }
+            else
+            {
+                throw new Exception("请填写正确的配置");
+            }
+
+            InitializeComponent();
+            this.AcceptButton = button1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataLoad();
+        }
+        public void DataLoad()
+        {
+            this.timer1.Start();
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = WinningPeoples;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var count = CandidateList.Count;
+            Random rd = new Random(DateTime.Now.Millisecond);
+            index = rd.Next(0, count);
+            textBox1.Text = CandidateList[index];
+        }
+
+        private void LuckyDraw()
+        {
+            textBox1.Text = "";
+            if (CandidateList.Count > 0)
+            {
+                this.timer1.Stop();
+                var data = CandidateList[index];
+                CandidateList.Remove(data);
+                foreach (var item in WinningPeoples)
+                {
+                    if (item.CodeReviewName == null)
+                    {
+                        item.CodeReviewName = data;
+                        DataLoad();
+                        return;
+                    }
+                    else if (item.updateCodeName == null)
+                    {
+                        item.updateCodeName = data;
+                        DataLoad();
+                        return;
+                    }
+                }
+                MessageBox.Show("本次抽奖结束");
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("本次抽奖结束");
+            }
+        }
+        public class WinningPeople
+        {
+            [DisplayName("Code Review")]
+            public string CodeReviewName { get; set; }
+            [DisplayName("Code Update")]
+            public string updateCodeName { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LuckyDraw();
+        }
+    }
+}
